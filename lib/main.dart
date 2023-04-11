1a0633c719afb68cabfd68f4f52b7f495c8bfc45
Philippe Gilbert <philippe@morialkar.com>
2023-04-11 11:50:44 -0400

    new js keywords,up pub packages,changed colors

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.amber,
+        primarySwatch: Colors.orange,
       ),
       home: const MyHomePage(title: 'Anonymisateur de code pour Chat GPT'),
     );
@@ -127,167 +127,188 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      floatingActionButton: () {
-        switch (_state) {
-          case AppState.anonymize:
-            return FloatingActionButton(
-                onPressed: () {
-                  setState(() {
-                    _state = AppState.deanonymize;
-                  });
-                },
-                child: const Icon(Icons.logout));
-
-          case AppState.deanonymize:
-            return FloatingActionButton(
-                onPressed: () {
-                  setState(() {
-                    _state = AppState.anonymize;
-                  });
-                },
-                child: const Icon(Icons.login));
-
-          case AppState.anonymizeResult:
-          case AppState.deanonymizeResult:
-            return FloatingActionButton(
-                onPressed: () {
-                  setState(() {
-                    _anonymizedValue = "";
-                    _deanonymizedString = "";
-                    _deanonymizeKeyString = "";
-                    anonymizeTextarea.text = "";
-                    deanonymizeTextarea.text = "";
-                    deanonymizeKeyTextarea.text = "";
-
-                    _state = _state == AppState.anonymizeResult
-                        ? AppState.anonymize
-                        : AppState.deanonymize;
-                  });
-                },
-                child: const Icon(Icons.refresh));
-          default:
-        }
-      }(),
-      body: Container(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: () {
-            switch (_state) {
-              case AppState.anonymize:
-                return <Widget>[
-                  Row(
-                    children: [
-                      const Text(
-                        "Entrez votre code pour l'anonymizer puis sélectionnez un language:",
-                      ),
-                      DropdownButton(
-                        items: _files,
-                        onChanged: (value) {
-                          setState(() {
-                            _selectedFile = value;
-                          });
-                        },
-                        value: _selectedFile,
-                        hint: const Text('Selectionnez un language'),
-                      ),
-                    ],
-                  ),
-                  TextField(
-                    controller: anonymizeTextarea,
-                    keyboardType: TextInputType.multiline,
-                    maxLines: 10,
-                  ),
-                  ElevatedButton(
-                      onPressed: anonymizeString,
-                      child: const Text("Cliquez pour anonymiser"))
-                ];
-              case AppState.deanonymize:
-                return <Widget>[
-                  const Text(
-                    "Entrez votre code pour le dé-anonymiser:",
-                  ),
-                  TextField(
-                    controller: deanonymizeTextarea,
-                    keyboardType: TextInputType.multiline,
-                    maxLines: 10,
-                  ),
-                  const Text(
-                    "Puis entrez votre clé de dé-anonymisation:",
-                  ),
-                  TextField(
-                    controller: deanonymizeKeyTextarea,
-                    keyboardType: TextInputType.multiline,
-                    maxLines: 2,
-                  ),
-                  ElevatedButton(
-                      onPressed: deanonymizeString,
-                      child: const Text("Cliquez pour dé-anonymiser"))
-                ];
-              case AppState.anonymizeResult:
-                return <Widget>[
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text("Votre code anonymisé"),
-                        ElevatedButton(
-                            onPressed: () => Clipboard.setData(
-                                ClipboardData(text: _anonymizedValue)),
-                            child: const Icon(Icons.copy_all))
-                      ]),
-                  Text(_anonymizedValue),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text("Votre clé de dé-anonymisation"),
-                        ElevatedButton(
-                            onPressed: () => Clipboard.setData(
-                                ClipboardData(text: _deanonymizeKeyString)),
-                            child: const Icon(Icons.copy_all))
-                      ]),
-                  Text(_deanonymizeKeyString),
-                ];
-              case AppState.deanonymizeResult:
-                return <Widget>[
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text("Votre code dé-anonymisé"),
-                        ElevatedButton(
-                            onPressed: () => Clipboard.setData(
-                                ClipboardData(text: _deanonymizedString)),
-                            child: const Icon(Icons.copy_all))
-                      ]),
-                  Text(_deanonymizedString),
-                ];
-              default:
-                return [const Text("_state error")];
-            }
-          }(),
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        floatingActionButton: () {
+          switch (_state) {
+            case AppState.anonymize:
+              return FloatingActionButton(
+                  onPressed: () {
+                    setState(() {
+                      _state = AppState.deanonymize;
+                    });
+                  },
+                  child: const Icon(Icons.logout));
+
+            case AppState.deanonymize:
+              return FloatingActionButton(
+                  onPressed: () {
+                    setState(() {
+                      _state = AppState.anonymize;
+                    });
+                  },
+                  child: const Icon(Icons.login));
+
+            case AppState.anonymizeResult:
+            case AppState.deanonymizeResult:
+              return FloatingActionButton(
+                  onPressed: () {
+                    setState(() {
+                      _anonymizedValue = "";
+                      _deanonymizedString = "";
+                      _deanonymizeKeyString = "";
+                      anonymizeTextarea.text = "";
+                      deanonymizeTextarea.text = "";
+                      deanonymizeKeyTextarea.text = "";
+
+                      _state = _state == AppState.anonymizeResult
+                          ? AppState.anonymize
+                          : AppState.deanonymize;
+                    });
+                  },
+                  child: const Icon(Icons.refresh));
+            default:
+          }
+        }(),
+        body: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          return Container(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              // Column is also a layout widget. It takes a list of children and
+              // arranges them vertically. By default, it sizes itself to fit its
+              // children horizontally, and tries to be as tall as its parent.
+              //
+              // Invoke "debug painting" (press "p" in the console, choose the
+              // "Toggle Debug Paint" action from the Flutter Inspector in Android
+              // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+              // to see the wireframe for each widget.
+              //
+              // Column has various properties to control how it sizes itself and
+              // how it positions its children. Here we use mainAxisAlignment to
+              // center the children vertically; the main axis here is the vertical
+              // axis because Columns are vertical (the cross axis would be
+              // horizontal).
+
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: () {
+                switch (_state) {
+                  case AppState.anonymize:
+                    return <Widget>[
+                      Row(
+                        children: [
+                          const Text(
+                            "Entrez votre code pour l'anonymizer puis sélectionnez un language:",
+                          ),
+                          DropdownButton(
+                            items: _files,
+                            onChanged: (value) {
+                              setState(() {
+                                _selectedFile = value;
+                              });
+                            },
+                            value: _selectedFile,
+                            hint: const Text('Selectionnez un language'),
+                          ),
+                        ],
+                      ),
+                      TextField(
+                        controller: anonymizeTextarea,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 10,
+                      ),
+                      ElevatedButton(
+                          onPressed: anonymizeString,
+                          child: const Text("Cliquez pour anonymiser"))
+                    ];
+                  case AppState.deanonymize:
+                    return <Widget>[
+                      const Text(
+                        "Entrez votre code pour le dé-anonymiser:",
+                      ),
+                      TextField(
+                        controller: deanonymizeTextarea,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 10,
+                      ),
+                      const Text(
+                        "Puis entrez votre clé de dé-anonymisation:",
+                      ),
+                      TextField(
+                        controller: deanonymizeKeyTextarea,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 2,
+                      ),
+                      ElevatedButton(
+                          onPressed: deanonymizeString,
+                          child: const Text("Cliquez pour dé-anonymiser"))
+                    ];
+                  case AppState.anonymizeResult:
+                    return <Widget>[
+                      Row(
+                        children: [
+                          Container(
+                              width: constraints.maxWidth * 0.4,
+                              child: SingleChildScrollView(
+                                  child: Column(children: [
+                                Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      const Text("Votre code anonymisé"),
+                                      ElevatedButton(
+                                          onPressed: () => Clipboard.setData(
+                                              ClipboardData(
+                                                  text: _anonymizedValue)),
+                                          child: const Icon(Icons.copy_all))
+                                    ]),
+                                Text(_anonymizedValue),
+                              ]))),
+                          Container(
+                              width: constraints.maxWidth * 0.4,
+                              child: SingleChildScrollView(
+                                  child: Column(children: [
+                                Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      const Text(
+                                          "Votre clé de dé-anonymisation"),
+                                      ElevatedButton(
+                                          onPressed: () => Clipboard.setData(
+                                              ClipboardData(
+                                                  text: _deanonymizeKeyString)),
+                                          child: const Icon(Icons.copy_all))
+                                    ]),
+                                Text(_deanonymizeKeyString),
+                              ]))),
+                        ],
+                      )
+                    ];
+                  case AppState.deanonymizeResult:
+                    return <Widget>[
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text("Votre code dé-anonymisé"),
+                            ElevatedButton(
+                                onPressed: () => Clipboard.setData(
+                                    ClipboardData(text: _deanonymizedString)),
+                                child: const Icon(Icons.copy_all))
+                          ]),
+                      Text(_deanonymizedString),
+                    ];
+                  default:
+                    return <Widget>[const Text("_state error")];
+                }
+              }(),
+            ),
+          ); // This trailing comma makes auto-formatting nicer for build methods.
+        }));
   }
 }
